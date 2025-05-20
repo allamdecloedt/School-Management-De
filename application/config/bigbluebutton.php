1dedffc20460cd81f8a6093397ead7a9f061a7b3
fettah1949 <fattahallam1949@gmail.com>
2025-05-20 17:19:57 +0100

    SM-master add bigbleu1

--- application/config/bigbluebutton.php
+++ application/config/bigbluebutton.php
@@ -6,9 +6,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 // $config['bbb_secret'] = 'mNPemKRmyLZlmVlQ4AafeUB5IBrHFYtfS5T3HU370';
 
 // version production
-$config['bbb_url'] = 'https://36788c46f0cb1289f19109fd5ad57c36.serveo.net/bigbluebutton/api/';
-$config['bbb_url_play'] = '36788c46f0cb1289f19109fd5ad57c36.serveo.net';
-$config['bbb_url_old'] = 'https://36788c46f0cb1289f19109fd5ad57c36.serveo.net:80/';
+$config['bbb_url'] = 'https://f90bf6773f3c9b1ceff10e29f34b8f5d.serveo.net/bigbluebutton/api/';
+$config['bbb_url_play'] = 'f90bf6773f3c9b1ceff10e29f34b8f5d.serveo.net';
+$config['bbb_url_old'] = 'https://f90bf6773f3c9b1ceff10e29f34b8f5d.serveo.net:80/';
 $config['bbb_secret'] = 'wgVh7JyComkib7tNlmt92GYUmGnpfJ73AXXqp4g6U';
 // version pre production
 // $config['bbb_url'] = 'https://hawaiian-poems-based-soundtrack.trycloudflare.com/bigbluebutton/api/';
