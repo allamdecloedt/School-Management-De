fcc5176629ec1bbb9eec25efb889537ff3a2e15b
fettah1949 <fattahallam1949@gmail.com>
2025-05-22 13:21:14 +0100

    SM-master  #comment big

--- application/config/bigbluebutton.php
+++ application/config/bigbluebutton.php
@@ -6,9 +6,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 // $config['bbb_secret'] = 'mNPemKRmyLZlmVlQ4AafeUB5IBrHFYtfS5T3HU370';
 
 // version production
-$config['bbb_url'] = 'https://f90bf6773f3c9b1ceff10e29f34b8f5d.serveo.net/bigbluebutton/api/';
-$config['bbb_url_play'] = 'f90bf6773f3c9b1ceff10e29f34b8f5d.serveo.net';
-$config['bbb_url_old'] = 'https://f90bf6773f3c9b1ceff10e29f34b8f5d.serveo.net:80/';
+$config['bbb_url'] = 'https://6d74-41-250-136-62.ngrok-free.app/bigbluebutton/api/';
+$config['bbb_url_play'] = 'https://6d74-41-250-136-62.ngrok-free.app';
+$config['bbb_url_old'] = 'https://6d74-41-250-136-62.ngrok-free.app:80/';
 $config['bbb_secret'] = 'wgVh7JyComkib7tNlmt92GYUmGnpfJ73AXXqp4g6U';
 // version pre production
 // $config['bbb_url'] = 'https://hawaiian-poems-based-soundtrack.trycloudflare.com/bigbluebutton/api/';
