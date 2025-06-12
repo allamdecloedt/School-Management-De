776c7d08eb48bf38291fac183027b5306bed9ad4
fettah1949 <fattahallam1949@gmail.com>
2025-06-12 11:41:57 +0100

    database humhub

--- application/config/database.php
+++ application/config/database.php
@@ -98,3 +98,25 @@ $db['default'] = array(
  
     
 );
+
+
+$db['humhub'] = [
+    'dsn'      => '',
+    'hostname' => '51.94.163.211',
+    'username' => 'root',
+    'password' => '123456789',
+    'database' => 'humhub',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'char_set' => 'utf8mb4',
+    'dbcollat' => 'utf8mb4_unicode_ci',
+    'swap_pre' => '',
+    'encrypt'  => FALSE,
+    'compress' => FALSE,
+    'strict_on'=> FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+];
